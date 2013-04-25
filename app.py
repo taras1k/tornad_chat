a336c05e5465c7651508d2ce36c026bb9670549b
Taras Nek <mr.taras1k@gmail.com>
2013-04-25 18:11:10 +0300

    fun fun broken)))

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+import random
+import json
+import uuid
 import tornado.httpserver
 import tornado.auth
 import tornado.web
@@ -15,19 +18,48 @@ TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
 c = tornadoredis.Client()
 c.connect()
 
+@tornado.gen.engine
+def init_data():
+    with c.pipeline() as pipe:
+        pipe.set('waiters', 1)
+        yield tornado.gen.Task(pipe.execute)
+
 class BaseHandler(tornado.web.RequestHandler):
 
     def get_current_user(self):
         user = self.get_secure_cookie('user')
         if user:
-            return user
+            return json.loads(user)
         return None
 
 class MainHandler(BaseHandler):
 
-    @tornado.web.authenticated
     def get(self):
         self.render('index.html', title='PubSub + WebSocket Demo')
+
+
+class StartChatHandler(BaseHandler):
+
+    @tornado.web.authenticated
+    @tornado.gen.engine
+    def get(self):
+        status = 'unjoined'
+        user = self.get_current_user()
+        prev_chater = self.get_secure_cookie('chater')
+        waiters = yield tornado.gen.Task(c.get, 'waiters')
+        self.write(status)
+#        if waiters:
+#            next_chater = random.choice(waiters)
+#            waiters.remove(next_chater)
+#            self.set_secure_cookie('chater', next_chater)
+#            status = 'joined'
+#        elif user['uui'] not in waiters:
+#            waiters.append(user['uuid'])
+#        if prev_chater:
+#            waiters.append(prev_chater)
+#        with c.pipeline() as pipe:
+#            pipe.set('waiters', waiters)
+#            yield tornado.gen.Task(pipe.execute)
 
 
 class NewMessage(BaseHandler):
@@ -35,8 +67,9 @@ class NewMessage(BaseHandler):
     @tornado.web.authenticated
     def post(self):
         message = self.get_argument('message')
-        user = self.get_current_user()
-        c.publish('test_channel', '%s: %s' %(user, message))
+        chater = self.get_secure_cookie('chater')
+        if chater:
+            c.publish(chater, message)
         #self.set_header('Content-Type', 'text/plain')
         #self.write('sent: %s' % (message,))
 
@@ -53,12 +86,13 @@ class GoogleLoginHandler(BaseHandler, tornado.auth.GoogleMixin):
         if not user:
             self.authenticate_redirect()
             return
-        self.set_secure_cookie('user', str(user))
+        user['uuid'] = uuid.uuid4().get_hex()
+        self.set_secure_cookie('user', json.dumps(user))
         self.redirect(self.get_argument('next', '/'))
         # Save the user with, e.g., set_secure_cookie()
 
 
-class MessagesCatcher(tornado.websocket.WebSocketHandler):
+class MessagesCatcher(BaseHandler, tornado.websocket.WebSocketHandler):
 
     def __init__(self, *args, **kwargs):
         super(MessagesCatcher, self).__init__(*args, **kwargs)
@@ -66,14 +100,17 @@ class MessagesCatcher(tornado.websocket.WebSocketHandler):
 
     @tornado.gen.engine
     def listen(self):
+        user = self.get_current_user()
         self.client = tornadoredis.Client()
-        self.client.connect()
-        yield tornado.gen.Task(self.client.subscribe, 'test_channel')
-        self.client.listen(self.on_message)
+        if user:
+            self.client.connect()
+            chanel_name = user['uuid']
+            yield tornado.gen.Task(self.client.subscribe, chanel_name)
+            self.client.listen(self.on_message)
 
     def on_message(self, msg):
         if msg.kind == 'message':
-            self.write_message(str(msg.body))
+            self.write_message(msg.body)
         if msg.kind == 'disconnect':
             # Do not forget to restart a listen loop
             # after a successful reconnect attempt.
@@ -86,7 +123,7 @@ class MessagesCatcher(tornado.websocket.WebSocketHandler):
 
     def on_close(self):
         if self.client.subscribed:
-            self.client.unsubscribe('test_channel')
+            self.client.unsubscribe(self.get_current_user()['uuid'])
             self.client.disconnect()
 
 settings = {
@@ -100,10 +137,12 @@ application = tornado.web.Application([
     (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': STATIC_PATH}),
     (r'/msg', NewMessage),
     (r'/login', GoogleLoginHandler),
-    (r'/track/', MessagesCatcher),
+    (r'/track', MessagesCatcher),
+    (r'/change_chater', StartChatHandler),
 ], **settings)
 
 if __name__ == '__main__':
+    init_data()
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(8888)
     print 'Demo is runing at 0.0.0.0:8888\nQuit the demo with CONTROL-C'
