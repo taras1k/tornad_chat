246c84759c32073f50ff4371d00d151ebbaba791
Taras Nek <mr.taras1k@gmail.com>
2013-05-29 17:16:53 +0300

    anc links

--- static/js/socket.js
+++ static/js/socket.js
@@ -37,8 +37,13 @@ function chat_started(){
   show_message('Connected.', 'info');
 }
 
+function replace_urls(s){
+  var pattern = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/g;
+  return s.replace(pattern, "<a href='$1'>$1</a>")
+}
+
 function show_message(message, status){
-  var msg = $('<p />').text(message);
+  var msg = $('<p />').text(replace_urls(message));
   if(status=='info'){
     msg.addClass('muted')
   } else if (status=='me'){00
