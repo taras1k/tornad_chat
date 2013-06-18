979c75c01b445d94f1b2155721f8a958dddf24fe
Taras Nek <mr.taras1k@gmail.com>
2013-06-18 15:33:05 +0300

    post images to chat

--- static/js/socket.js
+++ static/js/socket.js
@@ -40,8 +40,9 @@ function chat_started(){
 
 function show_message(message, status){
   var msg = $('<p />').addClass('lead').text(message);
-  var pattern = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/g;
-  msg.html(msg.html().replace(pattern, "<a href='$1'>$1</a>"))
+  var pattern =
+  /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))(jpg|gif|png|jpeg|bmp))/g;
+  msg.html(msg.html().replace(pattern, "<img src='$1' />"))
   if(status=='info'){
     msg.addClass('muted')
   } else if (status=='me'){00
